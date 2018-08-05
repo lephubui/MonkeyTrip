c4275be4752f3fbfaf64c19dfb4abb7830d84ae3
Le Bui <lebui2892@gmail.com>
2018-08-04 21:23:17 -0700

    First glance the google map api

--- server/app.js
+++ server/app.js
@@ -1,8 +1,33 @@
-var express = require('express')
-var app = express()
- 
+"use strict";
+
+const express = require('express');
+const app = express();
+
+const shared = require('./config/shared');
+
+const googleMapsClient = require('@google/maps').createClient({
+  key:shared.googleMapAPIKey,
+  Promise: Promise
+});
+
+var results; 
+googleMapsClient.geocode({address: '1600 Amphitheatre Parkway, Mountain View, CA'})
+  .asPromise()
+  .then((response) => {
+    results = response;
+    console.log(response.json.results);
+  })
+  .catch((err) => {
+    console.log('Error: ' + err);
+  });
+
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.send('Hello World');
+})
+
+app.get('/googleMap', function (req, res) {
+  res.send('Your in Google Map');
+  res.send(results);
 })
 console.log('Start Server at port 2892'); 
 app.listen(2892)
